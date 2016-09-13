4d50ba7bc4bc950e2bdae9d209da3f6ef0cd50a9
DanteLore <dan@logicalgenetics.com>
2016-09-13 12:20:27 +0100

    Refactored the concept of a slack channel out of the bot itself - so the bot just does chatting now

---
+++ bot/channel.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+class Channel:
+    def __init__(self, slack, channel, jira_id_to_attachment_converter, logger):
+        self.jira_id_to_attachment_converter = jira_id_to_attachment_converter
+        self.logger = logger
+        self.history = deque(maxlen=40)
+        self.slack = slack
+        self.channel = channel
+        self.channel_id = slack.get_channel_id(channel)
+        if self.channel_id:
+            self.logger.info("Operating on channel '{0}' (id:'{1}')".format(self.channel, self.channel_id))
+        else:
+            raise ValueError("Could not find a channel or group named {0}".format(channel))
+
+        self.bot_id = slack.get_user_id("JiraBot")
+        if self.bot_id:
+            self.logger.info("My name is JiraBot and my id is '{0}')".format(self.bot_id))
+        else:
+            raise ValueError("Can't look up bot id for 'JiraBot'")
+
+    def send(self, message, force, attachments=None):
+        if message not in self.history or force:
+            attachments = attachments or []
+            attachments += self.jira_id_to_attachment_converter.apply(message)
+            self.history.append(message)
+            self.slack.send(self.channel, message, attachments)
+
+    def get_messages(self):
+        my_id = self.bot_id.lower()
+        messages = self.slack.read_next_messages_for_channel(self.channel_id)
+        messages = filter(lambda m: "text" in m, messages)
+        for message in messages:
+            text = message["text"].lower()
+            user = message.get("user") or ""
+            self.logger.debug("Processing message: {0}".format(text))
+            if my_id in text:
+                self.logger.debug("I was mentioned")
+                yield text, user
--- bot/jirabot.py
+++ bot/jirabot.py
@@ -1,6 +1,6 @@
 import logging
-from collections import deque
 
+from bot.channel import Channel
 from bot.jira_query_executor import JiraQueryExecutor
 from bot.message_to_jira_attachment_converter import MessageToJiraAttachmentConverter
 from bot.warning_detector import JiraWarningDetector
@@ -15,41 +15,14 @@ class JiraBot:
             self.logger.setLevel("DEBUG")
             self.logger.addHandler(logging.StreamHandler())
 
-        self.history = deque(maxlen=40)
-        self.slack = slack
-        self.channel = channel
-        self.channel_id = slack.get_channel_id(channel)
-        if self.channel_id:
-            self.logger.info("Operating on channel '{0}' (id:'{1}')".format(self.channel, self.channel_id))
-        else:
-            raise ValueError("Could not find a channel or group named {0}".format(channel))
-
-        self.bot_id = slack.get_user_id("JiraBot")
-        if self.bot_id:
-            self.logger.info("My name is JiraBot and my id is '{0}')".format(self.bot_id))
-        else:
-            raise ValueError("Can't look up bot id for 'JiraBot'")
-
-        self.project = project
-        self.label = label
-        self.wip_limit = wip_limit
-        self.jira = jira.with_project(self.project).with_label(self.label)
-
-        self.warning_detector = JiraWarningDetector(self.jira, project, label, self.logger, wip_limit)
-        self.jira_id_to_attachment_converter = MessageToJiraAttachmentConverter(self.jira)
-        self.jira_executor = JiraQueryExecutor(self.jira)
+        jira = jira.with_project(project).with_label(label)
+        self.warning_detector = JiraWarningDetector(jira, project, label, self.logger, wip_limit)
+        self.jira_executor = JiraQueryExecutor(jira)
+        self.channel = Channel(slack, channel, MessageToJiraAttachmentConverter(jira), self.logger)
 
     def process_messages(self):
-        my_id = self.bot_id.lower()
-        messages = self.slack.read_next_messages_for_channel(self.channel_id)
-        messages = filter(lambda m: "text" in m, messages)
-        for message in messages:
-            text = message["text"].lower()
-            user = message.get("user") or ""
-            self.logger.debug("Processing message: {0}".format(text))
-            if my_id in text:
-                self.logger.debug("I was mentioned")
-                self.process_message_to_me(text, user)
+        for text, user in self.channel.get_messages():
+            self.process_message_to_me(text, user)
 
     def process_message_to_me(self, text, user):
         messages = []
@@ -71,10 +44,10 @@ class JiraBot:
 
         if len(messages) > 0:
             for message in messages:
-                self.send(self.channel, message, force=True)
+                self.channel.send(message, force=True)
         else:
-            self.send(self.channel, "Not sure what you mean by that <@{0}>, here's what I can do".format(user),
-                      force=True, attachments=[self.get_help_attachment()])
+            self.channel.send("Not sure what you mean by that <@{0}>, here's what I can do".format(user),
+                              force=True, attachments=[self.get_help_attachment()])
 
     def get_help_attachment(self):
         return {
@@ -93,7 +66,7 @@ class JiraBot:
         messages = self.get_warnings()
         messages += self.jira_executor.get_new_issues_on_backlog()
         for message in messages:
-            self.send(self.channel, message, force=False)
+            self.channel.send(message, force=False)
 
     def get_warnings(self):
         warnings = self.warning_detector.find_warnings()
@@ -106,10 +79,4 @@ class JiraBot:
         else:
             return warnings
 
-    def send(self, recipient, message, force, attachments=None):
-        msg = {"recipient": recipient, "message": message}
-        if msg not in self.history or force:
-            attachments = attachments or []
-            attachments += self.jira_id_to_attachment_converter.apply(message)
-            self.history.append(msg)
-            self.slack.send(recipient, message, attachments)
+
