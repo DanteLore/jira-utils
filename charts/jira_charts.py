1d08402392ab3242ca9f728fe72cba1a2c284da6
DanteLore <dan@logicalgenetics.com>
2016-09-26 18:26:05 +0100

    Added some caching for charts

--- charts/jira_charts.py
+++ charts/jira_charts.py
@@ -1,6 +1,8 @@
 import logging
-import tempfile
+import os
+import re
 from datetime import datetime, timedelta
+from distutils.dir_util import mkpath
 
 from charts.bar_chart import BarChart
 from charts.size_and_change_chart import SizeAndChangeChart
@@ -17,46 +19,53 @@ class JiraCharts:
             self.logger.setLevel("DEBUG")
             self.logger.addHandler(logging.StreamHandler())
 
-    def get_temp_filename(self):
-        return tempfile.mktemp(suffix=".png")
+    def get_temp_filename(self, title = "chart"):
+        path = datetime.now().strftime("/tmp/jira-charts/%Y/%m/%d")
+        mkpath(path)
+        title = re.sub("[^a-zA-Z0-9]", "_", title)
+        filename = "{0}/{1}.png".format(path, title)
+        return filename
 
-    def stories_closed_per_day(self, report_range=7):
-        filename = self.get_temp_filename()
+    def stories_closed_per_day(self, report_range=7, title="Stories Closed by Day", force=False):
+        filename = self.get_temp_filename("{0} {1}".format(title, report_range))
 
-        today = datetime.today()
-        offsets = [x for x in range(report_range, -1, -1) if (today - timedelta(days=x)).weekday() not in [5, 6]]
-        days = [(today - timedelta(days=x)).strftime('%A') for x in offsets]
-        issue_counts = [self.jira.resolved_n_days_ago(x).count_issues() for x in offsets]
+        if not os.path.exists(filename) or not force:
+            today = datetime.today()
+            offsets = [x for x in range(report_range, -1, -1) if (today - timedelta(days=x)).weekday() not in [5, 6]]
+            days = [(today - timedelta(days=x)).strftime('%A') for x in offsets]
+            issue_counts = [self.jira.resolved_n_days_ago(x).count_issues() for x in offsets]
 
-        chart = BarChart(days, issue_counts, title="Stories Closed by Day")
-        chart.save_to_file(filename)
+            chart = BarChart(days, issue_counts, title)
+            chart.save_to_file(filename)
 
         return filename
 
-    def stories_closed_per_week(self, report_range=6):
-        filename = self.get_temp_filename()
+    def stories_closed_per_week(self, report_range=6, title="Stories Closed by Week", force=False):
+        filename = self.get_temp_filename("{0} {1}".format(title, report_range))
 
-        today = datetime.today()
-        offsets = [x for x in range(report_range, -1, -1)]
-        dates = [(today - timedelta(days=x*7)).strftime('%d/%m') for x in offsets]
-        issue_counts = [self.jira.resolved_n_weeks_ago(x).count_issues() for x in offsets]
+        if not os.path.exists(filename) or not force:
+            today = datetime.today()
+            offsets = [x for x in range(report_range, -1, -1)]
+            dates = [(today - timedelta(days=x*7)).strftime('%d/%m') for x in offsets]
+            issue_counts = [self.jira.resolved_n_weeks_ago(x).count_issues() for x in offsets]
 
-        chart = BarChart(dates, issue_counts, title="Stories Closed by Week")
-        chart.save_to_file(filename)
+            chart = BarChart(dates, issue_counts, title)
+            chart.save_to_file(filename)
 
         return filename
 
-    def progress_by_day(self, report_range=31):
-        filename = self.get_temp_filename()
+    def progress_by_day(self, report_range=31, title="Stories Closed vs Stories Added by Day", force=False):
+        filename = self.get_temp_filename("{0} {1}".format(title, report_range))
 
-        today = datetime.today()
-        offsets = [x for x in range(report_range, -1, -1) if (today - timedelta(days=x)).weekday() not in [5, 6]]
-        days = [(today - timedelta(days=x)).strftime('%d/%m') for x in offsets]
-        size_data = [self.jira.open_issues_n_days_ago(x).count_issues() for x in offsets]
-        issues_closed = [self.jira.resolved_n_days_ago(x).count_issues() for x in offsets]
-        issues_added = [-1 * self.jira.created_n_days_ago(x).count_issues() for x in offsets]
+        if not os.path.exists(filename) or not force:
+            today = datetime.today()
+            offsets = [x for x in range(report_range, -1, -1) if (today - timedelta(days=x)).weekday() not in [5, 6]]
+            days = [(today - timedelta(days=x)).strftime('%d/%m') for x in offsets]
+            size_data = [self.jira.open_issues_n_days_ago(x).count_issues() for x in offsets]
+            issues_closed = [self.jira.resolved_n_days_ago(x).count_issues() for x in offsets]
+            issues_added = [-1 * self.jira.created_n_days_ago(x).count_issues() for x in offsets]
 
-        chart = SizeAndChangeChart(days, issues_closed, issues_added, size_data, title="Stories Closed vs Stories Added by Day")
-        chart.save_to_file(filename)
+            chart = SizeAndChangeChart(days, issues_closed, issues_added, size_data, title)
+            chart.save_to_file(filename)
 
         return filename
--- tests/chart_tests.py
+++ tests/chart_tests.py
@@ -19,17 +19,17 @@ class ChartTests(unittest.TestCase):
         stream_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
         logger.setLevel("DEBUG")
         logger.addHandler(stream_handler)
-        self.jira = Jira("https://comparethemarket.atlassian.net", logger=logger).with_project("BIB").with_label("BRO")
-        #self.jira = MockJira().with_n_fake_issues(50)
+        #self.jira = Jira("https://comparethemarket.atlassian.net", logger=logger).with_project("BIB").with_label("BRO")
+        self.jira = MockJira().with_n_fake_issues(50)
 
     def test_stories_closed_by_day(self):
-        filename = JiraCharts(self.jira).stories_closed_per_day()
+        filename = JiraCharts(self.jira).stories_closed_per_day(force=True)
         webbrowser.open("file://" + filename)
 
     def test_stories_closed_by_week(self):
-        filename = JiraCharts(self.jira).stories_closed_per_week()
+        filename = JiraCharts(self.jira).stories_closed_per_week(force=True)
         webbrowser.open("file://" + filename)
 
     def test_up_down_chat(self):
-        filename = JiraCharts(self.jira).progress_by_day()
+        filename = JiraCharts(self.jira).progress_by_day(force=True)
         webbrowser.open("file://" + filename)
