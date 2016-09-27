4884dc433fdb8ce9d24ffd71f9833f0738518baf
DanteLore <dan@logicalgenetics.com>
2016-09-27 09:23:25 +0100

    Slight tweak to progress chart axes

--- charts/size_and_change_chart.py
+++ charts/size_and_change_chart.py
@@ -12,18 +12,18 @@ class SizeAndChangeChart(JiraChart):
         self.positive_data = positive_data
 
     def save_to_file(self, filename):
-        fig, ax = plt.subplots(facecolor="white", figsize=(16, 6))
+        fig, ax1 = plt.subplots(facecolor="white", figsize=(16, 6))
 
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(True)
-        ax.spines['bottom'].set_visible(True)
-        ax.spines['left'].set_visible(True)
-        ax.xaxis.set_ticks_position('none')
-        ax.yaxis.set_ticks_position('left')
-        self.set_background_color(ax, "white")
-        self.set_foreground_color(ax, self.ctm_dark_blue)
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(True)
+        ax1.spines['bottom'].set_visible(True)
+        ax1.spines['left'].set_visible(True)
+        ax1.xaxis.set_ticks_position('none')
+        ax1.yaxis.set_ticks_position('left')
+        self.set_background_color(ax1, "white")
+        self.set_foreground_color(ax1, self.ctm_dark_blue)
 
-        ax2 = ax.twinx()
+        ax2 = ax1.twinx()
         self.set_background_color(ax2, "white")
         self.set_foreground_color(ax2, self.ctm_dark_blue)
         fig.patch.set_facecolor("white")
@@ -33,13 +33,14 @@ class SizeAndChangeChart(JiraChart):
 
         width = .9
         ind = np.arange(len(self.negative_data))
-        ax.bar(ind, self.negative_data, width=width, linewidth=0, facecolor="#CC0000")
+        ax1.bar(ind, self.negative_data, width=width, linewidth=0, facecolor="#CC0000")
         plt.xticks(ind + width / 2, self.x_data)
 
-        ax.bar(ind, self.positive_data, width=width, linewidth=0, facecolor=self.ctm_green)
+        ax1.bar(ind, self.positive_data, width=width, linewidth=0, facecolor=self.ctm_green)
+        ax1.set_ylim([min(self.negative_data) - 1, max(self.positive_data) + 1])
 
         plt.xticks(ind + width / 2, self.x_data)
-        ax2.plot(ax.get_xticks(), self.size_data, linestyle='-', linewidth=4.0, color=self.ctm_light_blue)
+        ax2.plot(ax1.get_xticks(), self.size_data, linestyle='-', linewidth=4.0, color=self.ctm_light_blue)
         ax2.set_ylim([0, max(self.size_data) + 5])
 
         if len(self.x_data) > 8:
--- tests/chart_tests.py
+++ tests/chart_tests.py
@@ -19,8 +19,8 @@ class ChartTests(unittest.TestCase):
         stream_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
         logger.setLevel("DEBUG")
         logger.addHandler(stream_handler)
-        #self.jira = Jira("https://comparethemarket.atlassian.net", logger=logger).with_project("BIB").with_label("BRO")
-        self.jira = MockJira().with_n_fake_issues(50)
+        self.jira = Jira("https://comparethemarket.atlassian.net", logger=logger).with_project("BIB").with_label("BRO")
+        #self.jira = MockJira().with_n_fake_issues(50)
 
     def test_stories_closed_by_day(self):
         filename = JiraCharts(self.jira).stories_closed_per_day(force=True)
